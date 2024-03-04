34646f22083d0263e0927d767bae85389b079a5c
stefan022 <stefan022github@gmail.com>
2024-03-04 18:56:40 +0100

    add avatar

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent, JSX } from "preact";
 
+import { Avatar } from "..";
+
 const Hero: FunctionComponent = (): JSX.Element => {
 	return (
 		<section className="w-full min-h-[800px] flex flex-col items-center justify-center relative z-10 gap-5">
-			<div>Avatar</div>
+			<Avatar />
 			<div>Info</div>
 			<div>Desc</div>
 			<div>ContactMe</div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { default as Navigation } from "./Navbar/Navigation/Navigation";
 export { default as MenuIcon } from "./Navbar/MenuIcon/MenuIcon";
 export { default as MenuContent } from "./Navbar/MenuContent/MenuContent";
 export { default as Hero } from "./Hero/Hero";
+export { default as Avatar } from "./Hero/Avatar/Avatar";
