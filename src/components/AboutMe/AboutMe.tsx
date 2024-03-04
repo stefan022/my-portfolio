c0aabd31f243f296db4af3068178361776bbc1ab
stefan022 <stefan022github@gmail.com>
2024-03-04 19:21:35 +0100

    add content

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent, JSX } from "preact";
 
 import { Title } from "..";
+import { aboutMe } from "../../constants/aboutMe.constant";
 
 const AboutMe: FunctionComponent = (): JSX.Element => {
 	return (
@@ -9,6 +10,18 @@ const AboutMe: FunctionComponent = (): JSX.Element => {
 			class="flex items-center justify-center flex-col relative z-10 gap-5 mb-32 pt-4"
 		>
 			<Title title="About Me" />
+
+			{aboutMe.map((value, index) => {
+				return (
+					<p
+						className="text-center text-gray-300"
+						data-aos="fade-up"
+						key={index}
+					>
+						{value}
+					</p>
+				);
+			})}
 		</section>
 	);
 };
