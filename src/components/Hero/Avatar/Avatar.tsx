83197aa63e293eb5e827b85cf2effac60b211109
stefan022 <stefan022github@gmail.com>
2024-03-05 00:51:07 +0100

    update logo & avatar

--- src/components/Hero/Avatar/Avatar.tsx
+++ src/components/Hero/Avatar/Avatar.tsx
@@ -4,12 +4,18 @@ import avatar from "../../../assets/avatar.webp";
 
 const Avatar: FunctionComponent = (): JSX.Element => {
 	return (
-		<div data-aos="fade-down">
+		<div
+			data-aos="fade-down"
+			className="relative rounded-full border-2 border-white w-[200px] max-sm:w-[150px] avatar"
+		>
 			<img
 				src={avatar}
 				alt="avatar.webp"
-				className="rounded-full border-2 border-white w-[200px] avatar max-sm:w-[150px]"
+				className="rounded-full relative z-10 avatar-img"
 			/>
+			<p className="absolute top-10 left-10 font-bold">
+				<span>Hi !</span>
+			</p>
 		</div>
 	);
 };
