4f524309e5b1a0a18a0c5288b945bd3f4289d9a9
stefan022 <stefan022github@gmail.com>
2024-03-04 18:17:30 +0100

    add navigation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent, JSX } from "preact";
 
-import { Logo } from "..";
+import { Logo, Navigation } from "..";
 
 const Navbar: FunctionComponent = (): JSX.Element => {
 	return (
 		<section className="w-10/12 mx-auto flex justify-between items-center py-8 relative z-30">
 			<Logo />
-			<div>Navigtion</div>
+			<Navigation />
 		</section>
 	);
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export { default as Navbar } from "./Navbar/Navbar";
 export { default as Logo } from "./Logo/Logo";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Navigation } from "./Navbar/Navigation/Navigation";
