cf08b17fc477b1ed5708214b8c5458f7d06b008b
stefan022 <stefan022github@gmail.com>
2024-03-04 17:43:34 +0100

    add scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { render } from "preact";
 
 import App from "./app.tsx";
 
+import "./scss/main.scss";
+
 const app = document.getElementById("app") as HTMLDivElement;
 
 render(<App />, app);
