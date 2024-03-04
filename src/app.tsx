8f133af187234bb0ed5ef31dc955194b260cc4d1
stefan022 <stefan022github@gmail.com>
2024-03-04 17:46:57 +0100

    init aos

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import { FunctionComponent, JSX } from "preact";
+import { useEffect } from "preact/hooks";
+
+import { aos } from "./utils/aos";
 
 const App: FunctionComponent = (): JSX.Element => {
+	useEffect(aos, []);
+
 	return <div>App</div>;
 };
 
