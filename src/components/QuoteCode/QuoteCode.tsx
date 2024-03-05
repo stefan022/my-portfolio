398b6ead0ee1c6c85892b5d5646a5dcab9931478
stefan022 <stefan022github@gmail.com>
2024-03-05 11:56:54 +0100

    update quote code

--- src/components/QuoteCode/QuoteCode.tsx
+++ src/components/QuoteCode/QuoteCode.tsx
@@ -2,11 +2,13 @@ import { FunctionComponent, JSX } from "preact";
 
 import { Title } from "../../components";
 
+import "./QuoteCode.scss";
+
 const QuoteCode: FunctionComponent = (): JSX.Element => {
 	return (
 		<section className="flex items-center justify-center flex-col relative z-10 gap-2.5 mb-32">
 			<Title title="Quote Code" />
-			<div className="flex flex-col" data-aos="fade-up">
+			<div className="flex flex-col relative code" data-aos="fade-up">
 				<code>
 					<span>do</span>
 					{` {`}
