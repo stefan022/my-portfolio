a4f4abda2490bf9e1319a8a1b5fdb35046850497
stefan022 <stefan022github@gmail.com>
2024-03-05 12:55:01 +0100

    refactor overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -4,14 +4,14 @@ import { useContext } from "preact/hooks";
 import { IMenuContext } from "../../typescript/interfaces/IMenuContext";
 import MenuContext from "../../context/MenuContext";
 
+import "./Overlay.scss";
+
 const Overlay: FunctionComponent = (): JSX.Element | null => {
 	const { isActive } = useContext(MenuContext as Context<IMenuContext>);
 
 	if (!isActive) return null;
 
-	return (
-		<div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-70 z-20"></div>
-	);
+	return <div className="overlay"></div>;
 };
 
 export default Overlay;
