268ebfdbe5ede5715b7159106c5cb389ee2b4219
stefan022 <stefan022github@gmail.com>
2024-03-05 13:02:00 +0100

    refactor container

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -1,11 +1,13 @@
 import { VNode, FunctionComponent, JSX } from "preact";
 
+import "./Container.scss";
+
 interface IProps {
 	children: VNode;
 }
 
 const Container: FunctionComponent<IProps> = ({ children }): JSX.Element => {
-	return <div className="max-w-[1080px] mx-auto max-xl:px-6">{children}</div>;
+	return <div className="container">{children}</div>;
 };
 
 export default Container;
