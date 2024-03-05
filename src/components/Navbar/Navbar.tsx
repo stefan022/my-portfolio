6c29c53a91c7d827d33a22ae2ce9a9230baab849
stefan022 <stefan022github@gmail.com>
2024-03-05 13:00:36 +0100

    refactor navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,11 @@ import { FunctionComponent, JSX } from "preact";
 
 import { Logo, MenuContent, MenuIcon, Navigation } from "..";
 
+import "./Navbar.scss";
+
 const Navbar: FunctionComponent = (): JSX.Element => {
 	return (
-		<section className="w-10/12 mx-auto flex justify-between items-center py-8 relative z-30">
+		<section className="navbar">
 			<Logo />
 			<Navigation />
 			<MenuIcon />
